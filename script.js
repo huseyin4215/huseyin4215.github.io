b7b72a11b90213a03cf94145f6b37430a65e348d
Hüseyin <saribugahuseyin770@gmail.com>
2025-04-07 16:42:53 +0300

    Update

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const projectBtn = document.querySelector(".btn-project");
     const contactBtn = document.querySelector(".btn-contact");
 
+    const dropHomePageBtn=document.querySelector(".DropBtn-homePage");
+    const dropaboutBtn=document.querySelector(".DropBtn-about");
+    const dropprojectBtn=document.querySelector(".DropBtn-project");
+    const dropcontactBtn=document.querySelector(".DropBtn-contact");
+
+
+
+
     const aboutSection = document.querySelector(".about-us");
     const projectSection = document.querySelector(".projects");
     const contactSection = document.querySelector(".contact-us");
@@ -28,5 +36,11 @@ document.addEventListener("DOMContentLoaded", function () {
         aboutSection.style.display="none";
         projectSection.style.display="none";
         contactSection.style.display="block";
+    });
+
+
+    dropHomePageBtn.addEventListener("click", function () {
+        window.location.href = "index.html";
+        projectBtn.click();
     });
 });
